b31e6a85830e8dd19fd8b0e5f33f5b4e33d52345
Malcolm McKellips <malcolmmckellips@gmail.com>
2023-02-12 13:50:33 -0700

    changed a3 makefile back to original as altering it did nothing to help autograder

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '4ada558bb26be4cfe63a00c802c486a3472fbc57'
+AESD_ASSIGNMENTS_VERSION = '900247a42dd03a9c1e0ce6bc3c770768b86789ec'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
