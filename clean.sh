a92085930a2c408b14bcb6f06ac73e98fcddccb3
Malcolm McKellips <malcolmmckellips@gmail.com>
2023-02-12 14:45:55 -0700

    Trying to fix runner

--- clean.sh
+++ clean.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Script to make distclean target from buildroot directory
 #Author: Malcolm McKellips
+#adding a line such that push will force github to rerun actions
 
 #select buildroot directory based on clean.sh location
 cd `dirname $0`/buildroot
