3958661ab2c77e09b56b24b6ed608632674ee330
Malcolm McKellips <malcolmmckellips@gmail.com>
2023-03-13 00:03:46 -0600

    Update make steps to follow BR module infrastructure. Set up overlay and init scripts. Succesfully loading modules in qemu. S98lddmodules has wrong name (will update after clean build)

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #Assignment 7 hash
-LDD_VERSION = '718cbdf07e082486e008537660e6b9fb4fb07a45'
+LDD_VERSION = 'f691cbaadcf38814b2a89174b53676b8ab8c5414'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -14,27 +14,9 @@ LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-malcolmmckellips.git'
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
-define LDD_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/scull
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/misc-modules
-endef
+#Let buildroot package infrastructure know which modules to build
+LDD_MODULE_SUBDIRS = scull
+LDD_MODULE_SUBDIRS += misc-modules
 
-# TODO: add misc-modules and scull components into the root filesystem.
-define LDD_INSTALL_TARGET_CMDS
-	#$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-	#$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-	#$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
-	#$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
-	#$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
-	#$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
-	#$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
-	#$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
-	$(INSTALL) -m 0755 $(@D)/scull/scull.ko $(TARGET_DIR)/usr/bin
-	#$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
-	#$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/misc-modules/*.ko $(TARGET_DIR)/usr/bin
-	#$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
-	#$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
-endef
-
+$(eval $(kernel-module))
 $(eval $(generic-package))
