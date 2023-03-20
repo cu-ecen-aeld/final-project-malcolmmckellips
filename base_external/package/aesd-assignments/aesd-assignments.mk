ecd7b9ec85f2dcaa24ef3014732b02c9e392379e
Malcolm McKellips <malcolmmckellips@gmail.com>
2023-03-20 16:14:11 -0600

    Update recipe that is working for socket test. Fails last test of driver test.

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -7,13 +7,17 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 #force another github action with this comment line
-AESD_ASSIGNMENTS_VERSION = '4b689cec7e9a02428176befbdd72f69cbd22caf0'
+AESD_ASSIGNMENTS_VERSION = 'fe6cc25042a9fa779b022a5a4a504f48da9852b3'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-malcolmmckellips.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+
+AESD_ASSIGNMENTS_MODULE_SUBDIRS = aesd-char-driver
+
+$(eval $(kernel-module))
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
@@ -30,6 +34,9 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/S97aesdcharmodules $(TARGET_DIR)/etc/init.d
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
