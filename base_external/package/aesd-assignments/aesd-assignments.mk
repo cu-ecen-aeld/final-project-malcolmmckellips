2de3f276c7e64cf425c855cd40f79845b06d90c3
Malcolm McKellips <malcolmmckellips@gmail.com>
2023-04-23 18:26:34 -0600

    add initial changes for i2c light sensor driver commit

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -7,7 +7,8 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 #force another github action with this comment line...
-AESD_ASSIGNMENTS_VERSION = '000373854b0dcf13557411120ca1c9b3b84ff457'
+#AESD_ASSIGNMENTS_VERSION = '000373854b0dcf13557411120ca1c9b3b84ff457' #last known working commit.
+AESD_ASSIGNMENTS_VERSION = 'eabb9633d0670b8fcb511f7c679db09cbb0a8052'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -22,6 +23,7 @@ $(eval $(kernel-module))
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/i2c all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
@@ -33,6 +35,7 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/i2c/i2clight $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/S97aesdcharmodules $(TARGET_DIR)/etc/init.d
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
